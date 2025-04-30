dde613b75a25ff940a7dacd7790cd3234f856a6a
Prathmesh Sankpal <prathmeshsankpal483@gmail.com>
2025-04-30 16:52:19 +0530

    Folder structure update

---
+++ src/app/components/subscription-card/subscription-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-subscription-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './subscription-card.component.html',
+  styleUrls: ['./subscription-card.component.css']
+})
+export class SubscriptionCardComponent {
+
+}
