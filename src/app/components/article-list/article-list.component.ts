5807da50c3a43324519ee50c2434392b6f05de41
Prathmesh Sankpal <prathmeshsankpal483@gmail.com>
2025-04-30 17:21:04 +0530

    News articles listing component added

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GlassArticle } from 'src/app/models/article.model';
+import { ArticleService } from 'src/app/services/article.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-article-list',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './article-list.component.html',
-  styleUrls: ['./article-list.component.css']
+  styleUrls: ['./article-list.component.css'],
 })
-export class ArticleListComponent {
+export class ArticleListComponent implements OnInit {
+  articles: GlassArticle[] = [];
+  liked: boolean = true;
 
+  constructor(private articleService: ArticleService, private router: Router) {}
+
+  ngOnInit() {
+    this.articleService.getArticles().subscribe((data) => {
+      this.articles = data.slice(0, 5); // show only first 5 articles
+    });
+  }
+
+  formatDate(dateStr: string): string {
+    return new Date(dateStr).toLocaleDateString();
+  }
+
+  showArticle(id: string) {
+    console.log('handled click');
+    this.router.navigate(['/news', id]);
+  }
 }
