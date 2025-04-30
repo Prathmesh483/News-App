60db91d217462675d28ec9316bf044dfa96fa84b
Prathmesh Sankpal <prathmeshsankpal483@gmail.com>
2025-04-30 17:38:32 +0530

    updated routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ArticleComponent } from './components/article/article.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/news', pathMatch: 'full' },
+  { path: 'news/:id', component: ArticleComponent },
+  { path: 'news', component: ArticleListComponent },
+];
