2bbacb4d8b7756ef8be09b72b62f6107ea57fbc0
Prathmesh Sankpal <prathmeshsankpal483@gmail.com>
2025-04-30 17:34:26 +0530

    updated main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SubscriptionCardComponent } from './components/subscription-card/subscription-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    SidebarComponent,
+    NavbarComponent,
+    SubscriptionCardComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'news-app';
