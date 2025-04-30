d60fc5f8ea002e7f4438745731196268ddadf604
Prathmesh Sankpal <prathmeshsankpal483@gmail.com>
2025-04-30 17:23:03 +0530

    articles service created

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { GlassArticle } from '../models/article.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ArticleService {
+  private apiUrl = 'https://glass-guru-backend.onrender.com/api/article';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getArticles(): Observable<GlassArticle[]> {
+    return this.http.get<GlassArticle[]>(this.apiUrl);
+  }
 }
