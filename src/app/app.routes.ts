e5fb1ec46fc523fc3817a761597a7735451e5709
Prathmesh Sankpal <prathmeshsankpal483@gmail.com>
2025-05-02 12:33:01 +0530

    routes update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { ArticleComponent } from './components/article/article.component';
 import { ArticleListComponent } from './components/article-list/article-list.component';
+import { EntertainmentComponent } from './components/entertainment/entertainment.component';
+import { HealthComponent } from './components/health/health.component';
+import { LifestyleComponent } from './components/lifestyle/lifestyle.component';
+import { AutosComponent } from './components/autos/autos.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/news', pathMatch: 'full' },
   { path: 'news/:id', component: ArticleComponent },
   { path: 'news', component: ArticleListComponent },
+  { path: 'entertainment', component: EntertainmentComponent },
+  { path: 'health', component: HealthComponent },
+  { path: 'lifestyle', component: LifestyleComponent },
+  { path: 'autos', component: AutosComponent },
 ];
