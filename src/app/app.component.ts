ea5d7a990bbd9bbf4d59f4f0d011b9f71244e32c
Prathmesh Sankpal <prathmeshsankpal483@gmail.com>
2025-05-02 12:32:26 +0530

    layout conditions update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SubscriptionCardComponent } from './components/subscription-card/subscription-card.component';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,6 +22,27 @@ import { SubscriptionCardComponent } from './components/subscription-card/subscr
 export class AppComponent {
   title = 'news-app';
   sidebarVisible = false;
+  largeSidebarVisible = true;
+  toggleVisible = true;
+
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    // Listen to the router's navigation events
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe(() => {
+        // Check the current route to hide sidebar on `/news/:id`
+        const currentRoute = this.router.url;
+        if (currentRoute.includes('/news/')) {
+          this.largeSidebarVisible = false;
+          this.toggleVisible = false;
+        } else {
+          this.largeSidebarVisible = true;
+          this.toggleVisible = true;
+        }
+      });
+  }
 
   toggleSidebar() {
     this.sidebarVisible = !this.sidebarVisible;
